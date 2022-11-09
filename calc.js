3faf1662ceecf252fc145b962e9b364c6d60f316
Ajose Michael Oluwatobi <ajosemichaeloluwatobi@yahoo.com>
2022-11-09 20:14:26 +0100

    eval commit

--- calc.js
+++ calc.js
@@ -5,63 +5,32 @@ const equal = document.getElementById('equal')
 let total = document.querySelector('.firstDiv')
 let operations = document.querySelector('.secDiv')
 let computation;
-let prev;
-let next;
-
-// const calculator = () => {
-//     updateOperand()
-//     // addition.addEventListener('click', (total, operations) => {
-//     //    operations.textContent = total.textContent += operations.textContent
-//     // })
-// }
-// calculator()
-// const update = () => {
-//     assignments.forEach(item => {
-//         item.addEventListener('click', function () {
-//             if (item.innerText = '/') {
-//                 total = prev.textContent / next.textContent
-//             } else if (item.innerText = '*') {
-//                 total = prev.textContent * next.textContent
-//             } else if (item.innerText = '+') {
-//                 total = prev.textContent + next.textContent
-//             } else {
-//                 total = prev.textContent - next.textContent
-//             }
-//         })
-//     })
-//     update();
-// }
-
-
-// function add(){
-//     operations.textContent = total.textContent
-//     operations.textContent = total.textContent += operations.textContent
-// }
 
 function updateOperand() {
     numbers.forEach(buttons => {
         buttons.addEventListener('click', (e) => {
-            computation = e.target.dataset.num
-            operations.textContent += computation
+            // computation = e.target.dataset.num
+            // operations.textContent += computation 
+            operations.innerText += e.target.innerText
         })
     })   
 }
 updateOperand()
 
 equal.addEventListener('click', function () {
-    if (operations.value === 0) {
-        return 0
-    } else {
-        total = eval(operations.value)
-        total = total
+    try {
+        total.innerText = eval(operations.innerText)
+        operations.innerText = total.innerText
+    } catch {
+        operations.innerText = 'Please enter a valid number'
     }
 })
 
 clearAll.addEventListener('click', () => {
     total.textContent = 0
-    operations.textContent = 0
+    operations.textContent = ''
 })
 
 deleteItem.addEventListener('click', () => {
-    total.textContent = total.textContent.toString().slice(0, -1)
+    operations.textContent = operations.textContent.slice(0, -1)
 })
