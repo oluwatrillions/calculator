d151eaf7bf19e4a40e216655fc5b49c2649047dc
Ajose Michael Oluwatobi <ajosemichaeloluwatobi@yahoo.com>
2022-11-09 07:04:05 +0100

    first commit calculator

--- calc.js
+++ calc.js
@@ -1,44 +1,67 @@
 const numbers = document.querySelectorAll('.numbers')
 const clearAll = document.getElementById('clear')
 const deleteItem = document.querySelector('.delete')
-const divide = document.querySelector('.divide')
-const multiply = document.querySelector('.multiply')
-const addition = document.querySelector('.addition')
-const subtract = document.querySelector('.subtract')
-const period = document.querySelector('.period')
 const equal = document.getElementById('equal')
-let firstOperand = document.querySelector('.firstDiv')
-let secOperand = document.querySelector('.secDiv')
+let total = document.querySelector('.firstDiv')
+let operations = document.querySelector('.secDiv')
 let computation;
+let prev;
+let next;
 
-const calculator = () => {
-    updateOperand()
-    addition.addEventListener('click', (firstOperand, secOperand) => {
-       secOperand.textContent = firstOperand.textContent += secOperand.textContent
-    })
-}
-calculator()
+// const calculator = () => {
+//     updateOperand()
+//     // addition.addEventListener('click', (total, operations) => {
+//     //    operations.textContent = total.textContent += operations.textContent
+//     // })
+// }
+// calculator()
+// const update = () => {
+//     assignments.forEach(item => {
+//         item.addEventListener('click', function () {
+//             if (item.innerText = '/') {
+//                 total = prev.textContent / next.textContent
+//             } else if (item.innerText = '*') {
+//                 total = prev.textContent * next.textContent
+//             } else if (item.innerText = '+') {
+//                 total = prev.textContent + next.textContent
+//             } else {
+//                 total = prev.textContent - next.textContent
+//             }
+//         })
+//     })
+//     update();
+// }
+
 
 // function add(){
-//     secOperand.textContent = firstOperand.textContent
-//     secOperand.textContent = firstOperand.textContent += secOperand.textContent
+//     operations.textContent = total.textContent
+//     operations.textContent = total.textContent += operations.textContent
 // }
 
 function updateOperand() {
     numbers.forEach(buttons => {
-        buttons.addEventListener('click', () => {
-            firstOperand.textContent = buttons.textContent.toString;
-            secOperand.textContent = firstOperand.textContent
+        buttons.addEventListener('click', (e) => {
+            computation = e.target.dataset.num
+            operations.textContent += computation
         })
     })   
 }
 updateOperand()
 
+equal.addEventListener('click', function () {
+    if (operations.value === 0) {
+        return 0
+    } else {
+        total = eval(operations.value)
+        total = total
+    }
+})
+
 clearAll.addEventListener('click', () => {
-    firstOperand.textContent = ''
-    secOperand.textContent = ''
+    total.textContent = 0
+    operations.textContent = 0
 })
 
 deleteItem.addEventListener('click', () => {
-    firstOperand.textContent = firstOperand.textContent.toString().slice(0, -1)
+    total.textContent = total.textContent.toString().slice(0, -1)
 })
