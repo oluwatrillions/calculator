992ae742a70d75a94db980dec3a9fc4d5149883b
Ajose Michael Oluwatobi <ajosemichaeloluwatobi@yahoo.com>
2022-11-10 19:46:28 +0100

    another commit

--- calc.js
+++ calc.js
@@ -1,26 +1,54 @@
 const numbers = document.querySelectorAll('.numbers')
+const assignments = document.querySelectorAll('.assignment')
 const clearAll = document.getElementById('clear')
 const deleteItem = document.querySelector('.delete')
 const equal = document.getElementById('equal')
 let total = document.querySelector('.firstDiv')
 let operations = document.querySelector('.secDiv')
-let computation;
+let tempDisplay = ''
+let totalDisplay = ''
+let result = null
+let nextOperation = ''
+let dot = false;
 
 function updateOperand() {
     numbers.forEach(buttons => {
         buttons.addEventListener('click', (e) => {
             // computation = e.target.dataset.num
             // operations.textContent += computation 
-            operations.innerText += e.target.innerText
+            // operations.innerText += e.target.innerText
+            if (e.target.innerText === '.' && !dot) {
+                dot = true;
+            } else if (e.target.innerText === '.' && dot) {
+                return;
+            }
+
+            totalDisplay += e.target.innerText
+            total.innerText = totalDisplay
         })
     })   
 }
 updateOperand()
 
-equal.addEventListener('click', function () {
+assignments.forEach(keys => {
+    keys.addEventListener('click', (e) => {
+        if (!totalDisplay)
+            return;
+        dot = false;
+        const operator = e.target.innerText
+        if (tempDisplay && totalDisplay) {
+            computation();
+        } else {
+            result = parseFloat(totalDisplay)
+        }
+        
+    })
+})
+
+equal.addEventListener('click', () => {
     try {
         total.innerText = eval(operations.innerText)
-        operations.innerText = total.innerText
+        operations.innerText = total.innerText 
     } catch {
         operations.innerText = 'Please enter a valid number'
     }
